5493a5618433b8111b61a620508e895aaf9389a3
Mohit Hasan <mohitmd09@gmail.com>
2022-10-11 01:18:18 +0600

    Create isPrimeByForLoop.kt
    This is a problem given in the class. I solved it by for loop in kotlin.

---
+++ Kotlin/isPrimeByForLoop.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    var isPrime = true
+
+    print("Enter number: ")
+    val num = readLine()!!.toInt()
+
+    for (a in num-1 downTo 2 ){
+
+        if (num%a==0){
+            isPrime = false
+            break
+        }
+    }
+    if (isPrime){
+        println("$num is a prime number")
+    } else {
+        println("$num is not a prime number")
+    }
+}
