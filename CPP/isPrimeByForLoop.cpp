dd1929baadfd2a24409f7c110e3aae5d628033fb
Mohit Hasan <mohitmd09@gmail.com>
2022-10-11 01:39:57 +0600

    Create isPrimeByForLoop.cpp
    Solved isPrime or not problem using for loop by cpp.

---
+++ CPP/isPrimeByForLoop.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num,a;
+    bool isPrime = true;
+   
+    cout << "Enter number: ";
+    cin >> num;
+    
+    for(a=num-1; a>=2; a--){
+        //printf("%d\n",a);
+        if(num%a==0){
+            isPrime = false;
+            break;
+        }
+    }
+    
+    if(isPrime){
+        printf("%d is a prime number",num);
+    } else if(!isPrime) {
+        printf("%d is not a prime number",num);
+    } else {
+        printf("Something went wrong!");
+    }
+}
