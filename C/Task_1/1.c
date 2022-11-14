ebb6c35c335cb0303d98bfb2ac8d3fcda68d96e5
Mohit Hasan <mohitmd09@gmail.com>
2022-11-15 02:43:41 +0600

    Create 1.c

---
+++ C/Task_1/1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+    int rows,colums,i,j;
+
+    printf("Enter rows and colums: ");
+    scanf("%d%d",&rows,&colums);
+
+    int A[rows][colums];
+
+    for(i=0; i<rows; i++){
+
+        for(j=0; j<colums; j++){
+                printf("A[%d][%d]: ",i,j);
+                scanf("%d",&A[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<rows; i++){
+
+        for(j=0; j<colums; j++){
+                printf("A[%d][%d]: %d ",i,j,A[i][j]);
+        }
+        printf("\n");
+    }
+
+}
